8e965c76a2839acb5830d6775b5c80ef86f3eb5f
Toktamysov Timur <timur.toktamysov@gmail.com>
2026-02-15 21:28:44 +0300

    refactoring

--- app/feature_extraction.py
+++ app/feature_extraction.py
@@ -1,8 +1,39 @@
 import numpy as np
+from scipy import signal
 from .entropy_complexity import compute_entropy_complexity
 
-# Note: Preprocessing functions (extract_frequency_features, detect_whistles) 
-# are in colab_utils.py for use in Google Colab
+
+def extract_frequency_features(audio_data, sample_rate, n_fft=2048):
+    """Extract frequency domain features."""
+    fft_result = np.fft.rfft(audio_data, n=n_fft)
+    frequencies = np.fft.rfftfreq(n_fft, 1/sample_rate)
+    magnitudes = np.abs(fft_result)
+    
+    feature_vector = {
+        'spectral_centroid': np.sum(frequencies * magnitudes) / np.sum(magnitudes),
+        'spectral_bandwidth': np.sqrt(np.sum(((frequencies - np.sum(frequencies * magnitudes) / np.sum(magnitudes)) ** 2) * magnitudes) / np.sum(magnitudes)),
+        'spectral_rolloff': frequencies[np.where(np.cumsum(magnitudes) >= 0.85 * np.sum(magnitudes))[0][0]],
+        'low_freq_energy': np.sum(magnitudes[frequencies < 500]),
+        'mid_freq_energy': np.sum(magnitudes[(frequencies >= 500) & (frequencies < 1000)]),
+        'high_freq_energy': np.sum(magnitudes[frequencies >= 1000]),
+        'peak_frequency': frequencies[np.argmax(magnitudes)]
+    }
+    
+    return frequencies, magnitudes, feature_vector
+
+
+def detect_whistles(audio_data, sample_rate, whistle_freq_range=(400, 1600)):
+    """Detect whistle sounds in audio."""
+    f, t, Sxx = signal.spectrogram(audio_data, sample_rate, nperseg=256)
+    
+    freq_mask = (f >= whistle_freq_range[0]) & (f <= whistle_freq_range[1])
+    whistle_energy = np.sum(Sxx[freq_mask, :])
+    total_energy = np.sum(Sxx)
+    
+    whistle_strength = whistle_energy / total_energy if total_energy > 0 else 0
+    whistle_detected = whistle_strength > 0.2
+    
+    return whistle_detected, whistle_strength
 
 
 def extract_all_features(signals, sample_rate, m=3, tau=1):
